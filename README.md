7a17cd8e7f199be40b9e0e45d22ab04e48a5f3ea
francorinque <francoagustinrinquecorreo@gmail.com>
2023-12-15 18:27:27 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+[] Implement Search
+  -Search by prompt
+  - Search by tag
+  -Search by username
+
+[] Implement Click on tag
+[] Implement view other profiles
---
+++ app/profile/[id]/page.jsx
@@ -0,0 +1,32 @@
+"use client"
+
+import { useEffect, useState } from "react"
+import { useSearchParams } from "next/navigation"
+
+const UserProfile = ({ params }) => {
+  const [userPosts, setUserPosts] = useState([])
+
+  const searchParams = useSearchParams()
+  const userName = searchParams.get("name")
+
+  const fetchPosts = async () => {
+    const response = await fetch(`/api/users/${params?.id}/posts`)
+    const data = await response.json()
+
+    setUserPosts(data)
+  }
+
+  useEffect(() => {
+    if (params?.id) fetchPosts()
+  }, [params.id])
+  return (
+    <>
+      <Profile
+        name={userName}
+        desc={`Welcome to ${userName}'s personalized profile page. Explore ${userName}'s exceptional prompts and be inspired by the power of their imagination`}
+        data={userPosts}
+      />
+    </>
+  )
+}
+export default UserProfile
